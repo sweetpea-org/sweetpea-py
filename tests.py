95e23cf16307165ebd1baded40d33d705fc88038
Ben Draut <drautb@gmail.com>
2018-06-05 17:26:26 -0600

    Make existing unit tests pass

--- tests.py
+++ tests.py
@@ -5,24 +5,27 @@ from FrontEnd import *
 import operator as op
 
 
- 
+
 # done
 def test_fullycrosssize():
     color = Factor("color", ["red", "blue"])
     text  = Factor("text",  ["red"])
     size  = Factor("size",  ["big", "small", "tiny"])
-    assert fullyCrossSize([color, color]) == 4
-    assert fullyCrossSize([color, color, color]) == 8
-    assert fullyCrossSize([size, text]) == 3
-    assert fullyCrossSize([size, color]) == 6
-    assert fullyCrossSize([text]) == 1
-    
-# needs more tests  
+    assert fully_cross_size([color, color]) == 4
+    assert fully_cross_size([color, color, color]) == 8
+    assert fully_cross_size([size, text]) == 3
+    assert fully_cross_size([size, color]) == 6
+    assert fully_cross_size([text]) == 1
+
+# needs more tests
 def test_get_depxProduct():
     color = Factor("color", ["red", "blue"])
     text  = Factor("text",  ["red", "blue"])
     conLevel  = DerivedLevel("con", WithinTrial(op.eq, [color, text]))
-    assert get_dep_xProduct(conLevel) == [('red', 'red'), ('red', 'blue'), ('blue', 'red'), ('blue', 'blue')]
-    
+    assert get_dep_xProduct(conLevel) == [(('color', 'red'), ('text', 'red')),
+                                          (('color', 'red'), ('text', 'blue')),
+                                          (('color', 'blue'), ('text', 'red')),
+                                          (('color', 'blue'), ('text', 'blue'))]
+
 def test_etc():
     assert None == None
