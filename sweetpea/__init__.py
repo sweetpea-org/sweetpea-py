e053a85b729ec3f7c43af06caa317b1a0db446db
Ben Draut <drautb@gmail.com>
2018-10-15 22:17:09 -0600

    Implement helper fn for variable visualization
    The `print_variable_grid` function can be used to produce a grid that
    shows the correlation between each boolean variable and experiment
    values. For example, for the 2-stroop test, it would produce something
    like this:
    
    ```
    ----------------------------------------------
    |   Trial |  color   |   text   | congruent? |
    |       # | red blue | red blue |  con  inc  |
    ----------------------------------------------
    |       1 |  1   2   |  3   4   |   5    6   |
    |       2 |  7   8   |  9   10  |  11    12  |
    |       3 | 13   14  | 15   16  |  17    18  |
    |       4 | 19   20  | 21   22  |  23    24  |
    ----------------------------------------------
    ```

--- sweetpea/__init__.py
+++ sweetpea/__init__.py
@@ -566,6 +566,70 @@ def fully_cross_block(design: List[Factor], xing: List[Factor], constraints: Any
 
 
 """
+Helper method to print a chart outlining the variable mappings, helpful for visualizing
+the formula space. For example, for the simple stroop test:
+----------------------------------------------
+|   Trial |  color   |   text   | congruent? |
+|       # | red blue | red blue |  con  inc  |
+----------------------------------------------
+|       1 |  1   2   |  3   4   |   5    6   |
+|       2 |  7   8   |  9   10  |  11    12  |
+|       3 | 13   14  | 15   16  |  17    18  |
+|       4 | 19   20  | 21   22  |  23    24  |
+----------------------------------------------
+"""
+def print_variable_grid(blk: HLBlock):
+    design_size = __design_size(blk.design)
+    num_trials = __fully_cross_size(blk.xing)
+    num_vars = __encoding_variable_size(blk.design, blk.xing)
+
+    largest_number_len = len(str(num_vars))
+
+    header_widths = []
+    row_format_str = '| {:>7} |'
+    for f in blk.design:
+        # length of all levels concatenated for this factor
+        level_names = list(map(__get_level_name, f.levels))
+        level_name_widths = [max(largest_number_len, l) for l in list(map(len, level_names))]
+
+        level_names_width = sum(level_name_widths) + len(level_names) - 1 # Extra length for spaces in between names.
+        factor_header_width = max(len(f.name), level_names_width)
+        header_widths.append(factor_header_width)
+
+        # If the header is longer than the level widths combined, then they need to be lengthened.
+        diff = factor_header_width - level_names_width
+        if diff > 0:
+            idx = 0
+            while diff > 0:
+                level_name_widths[idx] += 1
+                idx += 1
+                diff -= 1
+                if idx >= len(level_name_widths):
+                    idx = 0
+
+        # While we're here, build up the row format str.
+        row_format_str = reduce(lambda a, b: a + ' {{:^{}}}'.format(b), level_name_widths, row_format_str)
+        row_format_str += ' |'
+
+    header_format_str = reduce(lambda a, b: a + ' {{:^{}}} |'.format(b), header_widths, '| {:>7} |')
+    factor_names = list(map(lambda f: f.name, blk.design))
+    header_str = header_format_str.format(*["Trial"] + factor_names)
+    row_width = len(header_str)
+    print('-' * row_width)
+    print(header_str)
+
+    all_level_names = [ln for (fn, ln) in __get_all_level_names(blk.design)]
+    print(row_format_str.format(*['#'] + all_level_names))
+    print('-' * row_width)
+
+    for t in range(num_trials):
+        args = [str(t + 1)] + list(map(str, range(t * design_size + 1, t * design_size + design_size + 2)))
+        print(row_format_str.format(*args))
+
+    print('-' * row_width)
+
+
+"""
 Display the generated experiments in human-friendly form.
 """
 def print_experiments(hl_block: HLBlock, experiments: List[dict]):
