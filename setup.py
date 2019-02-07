ca88d3246c18d31d6b15cfe5488af14c7796a72d
Ben Draut <drautb@gmail.com>
2019-02-06 21:24:04 -0700

    Release new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sweetpea",
-    version="0.0.23",
+    version="0.0.24",
     author="Annie Cherkaev, Ben Draut",
     author_email="annie.cherk@gmail.com, drautb@cs.utah.edu",
     description="A language for synthesizing randomized experimental designs",
