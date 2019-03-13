709a78edfaaf3099e59324a0c56adef4caf3189f
Ben Draut <drautb@gmail.com>
2019-03-12 21:39:55 -0600

    Finish basic combinatoric construction strategy
    Seems to be working well so far, still want to add ATs.
    
    Still doesn't account for constraints at all.

--- sweetpea/sampling_strategies/uniform_combinatoric.py
+++ sweetpea/sampling_strategies/uniform_combinatoric.py
@@ -1,10 +1,13 @@
+import numpy as np
 import operator as op
 
 from functools import reduce
 from itertools import product
 from math import factorial
+from typing import List, cast
 
 from sweetpea.blocks import Block, FullyCrossBlock
+from sweetpea.combinatorics import extract_components, compute_jth_inversion_sequence, construct_permutation, compute_jth_combination
 from sweetpea.design_partitions import DesignPartitions
 from sweetpea.primitives import get_level_name
 from sweetpea.sampling_strategies.base import SamplingStrategy, SamplingResult
@@ -55,12 +58,75 @@ class UCSolutionEnumerator():
         self._block = block
         self._partitions = DesignPartitions(block)
         self._crossing_instances = self.__generate_crossing_instances()
+        self._source_combinations = self.__generate_source_combinations()
+        self._segment_lengths = cast(List[int], []) # Will be populated by solution counting
+
+        # Maintains a lookup for valid source combinations for a permutation.
+        # Example [[2, 3], ...] means that for permutation 0, indices 2 and 3 in the source combinations
+        # list are allowed.
+        self._valid_source_combinations = cast(List[List[int]], []) # Will be populated by solution counting
 
         # Needs to be called last.
         self._solution_count = self.__count_solutions()
 
     def solution_count(self):
         return self._solution_count
+
+    def generate_random_sample(self) -> List[int]:
+        # Select a random number from the range of solutions.
+        sequence_number = np.random.randint(0, self._solution_count)
+        return self.generate_sample(sequence_number)
+
+    def generate_sample(self, sequence_number: int) -> List[int]:
+        # 1. Extract the component pieces (permutation, each combination setting, etc)
+        #    The 0th component is always the permutation index.
+        #    The 1st-nth components are always the source combination indices.
+        #    Any following components are the combination indices for independent basic factors.
+        components = extract_components(self._segment_lengths, sequence_number)
+
+        # 2. Generate the inversion sequence for the selected permutation number.
+        #    Use the inversion sequence to construct the permutation.
+        l = self._block.crossing_size()
+        inversion_sequence = compute_jth_inversion_sequence(l, components[0])
+        permutation_indices = construct_permutation(inversion_sequence)
+        permutation = list(map(lambda i: self._crossing_instances[i], permutation_indices))
+
+        # 3. Generate the source combinations for the selected sequence.
+        source_combinations = cast(List[dict], [])
+        for i, p in enumerate(permutation_indices):
+            source_combinations.append(self._source_combinations[self._valid_source_combinations[p][components[i + 1]]])
+
+        # 4. Generate the combinations for independent basic factors
+        independent_factor_combinations = cast(List[List[dict]], [[{}]] * l)
+        u_b_i = self._partitions.get_uncrossed_basic_independent_factors()
+        for f_idx, independent_combination_idx in enumerate(components[l + 1:]):
+            f = u_b_i[f_idx]
+            combo = compute_jth_combination(l, len(f.levels), independent_combination_idx)
+            combo_dicts = [{f.name: f.levels[level_idx] for level_idx in combo}]
+            independent_factor_combinations[f_idx] = combo_dicts
+
+        # 5. Merge the selected levels gathered so far to facilitate computing the uncrossed derived factor levels.
+        trial_values = cast(List[dict], [{}] * l)
+        for t in range(l):
+            i_d_f = {k: v for d in independent_factor_combinations[t] for k, v in d.items()}
+            trial_values[t] = {**permutation[t], **source_combinations[t], **i_d_f}
+
+        # 6. Generate uncrossed derived level values
+        u_d = self._partitions.get_uncrossed_derived_factors()
+        for f in u_d:
+            for t in range(l):
+                # For each level in the factor, see if the derivation function is true.
+                for level in f.levels:
+                    if level.window.fn(*[trial_values[t][f.name] for f in level.window.args]):
+                        trial_values[t][f.name] = level.name
+
+        # 7. Convert to variable encoding for SAT checking
+        solution = cast(List[int], [])
+        for trial_number, trial_value in enumerate(trial_values):
+            for factor_name, level_name in trial_value.items():
+                solution.append(self._block.get_variable(trial_number + 1, (factor_name, level_name)))
+
+        return solution
 
     """
     Generates all the crossings, indexed by factor name for easy lookup later.
@@ -69,47 +135,48 @@ class UCSolutionEnumerator():
         ...
     ]
     """
-    def __generate_crossing_instances(self):
+    def __generate_crossing_instances(self) -> List[dict]:
         crossing = self._partitions.get_crossed_factors()
         level_lists = [list(map(get_level_name, f.levels)) for f in crossing]
         return [{crossing[i].name: level for i,level in enumerate(levels)} for levels in product(*level_lists)]
 
-    def __get_source_combinations(self):
+    def __generate_source_combinations(self) -> List[dict]:
         ubs = self._partitions.get_uncrossed_basic_source_factors()
         level_lists = [list(map(get_level_name, f.levels)) for f in ubs]
         return [{ubs[i].name: level for i,level in enumerate(levels)} for levels in product(*level_lists)]
 
     def __count_solutions(self):
+        self._segment_lengths = []
         ##############################################################
         # Permutations of crossing instances
         n = self._block.crossing_size()
         n_factorial = factorial(n)
+        self._segment_lengths.append(n_factorial)
 
         ##############################################################
         # Uncrossed Dependent Factors
-        level_combinations = self.__get_source_combinations()
+        level_combinations = self.__generate_source_combinations()
 
-        ci_level_combos = []
         # Keep only allowed combos for each permutation
         for ci in self._crossing_instances:
-            lc_copy = level_combinations.copy()
-            for lc in level_combinations:
+            sc_indices = list(range(len(self._source_combinations)))
+            for sc_idx, sc in enumerate(self._source_combinations):
                 # Apply the derivation fn for each DF in the crossing for this instance and make sure it returns
                 # true for this level combination. If it doesn't, then remove this combination.
-                merged_levels = {**ci, **lc}
+                merged_levels = {**ci, **sc}
                 for df in self._partitions.get_crossed_factors_derived():
                     w = df.get_level(merged_levels[df.name]).window
                     if not w.fn(*[merged_levels[f.name] for f in w.args]):
-                        lc_copy.remove(lc)
+                        sc_indices.remove(sc_idx)
 
-            ci_level_combos.append(lc_copy)
+            self._segment_lengths.append(len(sc_indices))
+            self._valid_source_combinations.append(sc_indices)
 
         ##############################################################
         # Uncrossed Independent Factors
         u_b_i_counts = []
         u_b_i = self._partitions.get_uncrossed_basic_independent_factors()
         for f in u_b_i:
-            u_b_i_counts.append(pow(len(f.levels), n))
+            self._segment_lengths.append(pow(len(f.levels), n))
 
-        count = n_factorial * reduce(op.mul, map(len, ci_level_combos), 1) * reduce(op.mul, u_b_i_counts, 1)
-        return count
+        return reduce(op.mul, self._segment_lengths, 1)
--- sweetpea/tests/sampling_strategies/test_uc_solution_enumerator.py
+++ sweetpea/tests/sampling_strategies/test_uc_solution_enumerator.py
@@ -1,4 +1,5 @@
 import operator as op
+import pytest
 
 from sweetpea import fully_cross_block
 from sweetpea.primitives import Factor, DerivedLevel, WithinTrial
@@ -32,9 +33,77 @@ def test_generate_source_combinations():
     block = fully_cross_block(design, [congruency], [])
     enumerator = UCSolutionEnumerator(block)
 
-    assert enumerator._UCSolutionEnumerator__get_source_combinations() == [
+    assert enumerator._UCSolutionEnumerator__generate_source_combinations() == [
         {'color': 'red', 'text': 'red'},
         {'color': 'red', 'text': 'blue'},
         {'color': 'blue', 'text': 'red'},
         {'color': 'blue', 'text': 'blue'}
     ]
+
+
+@pytest.mark.parametrize('sequence_number, expected_solution', [
+    [0,  [1, 3, 5, 7, 10, 12, 14, 15, 18, 20, 22, 23]],
+    [1,  [1, 4, 6, 7, 9,  11, 14, 15, 18, 20, 22, 23]],
+    [2,  [1, 4, 6, 8, 9,  12, 13, 15, 17, 20, 22, 23]],
+    [3,  [1, 4, 6, 8, 9,  12, 14, 16, 17, 19, 21, 23]],
+    [4,  [1, 3, 5, 8, 9,  12, 13, 16, 18, 20, 22, 23]],
+    [5,  [2, 3, 6, 7, 9,  11, 13, 16, 18, 20, 22, 23]],
+    [6,  [2, 3, 6, 7, 10, 12, 13, 15, 17, 20, 22, 23]],
+    [7,  [2, 3, 6, 7, 10, 12, 14, 16, 17, 19, 21, 23]],
+    [8,  [1, 3, 5, 8, 9,  12, 14, 16, 17, 19, 22, 24]],
+    [9,  [2, 3, 6, 7, 9,  11, 14, 16, 17, 19, 22, 24]],
+    [10, [2, 3, 6, 8, 10, 11, 13, 15, 17, 19, 22, 24]],
+    [11, [2, 3, 6, 8, 10, 11, 13, 16, 18, 19, 21, 23]],
+    [12, [1, 3, 5, 7, 10, 12, 14, 16, 17, 20, 21, 24]],
+    [13, [1, 4, 6, 7, 9,  11, 14, 16, 17, 20, 21, 24]],
+    [14, [1, 4, 6, 8, 10, 11, 13, 15, 17, 20, 21, 24]],
+    [15, [1, 4, 6, 8, 10, 11, 14, 15, 18, 19, 21, 23]],
+    [16, [1, 3, 5, 8, 10, 11, 13, 16, 18, 20, 21, 24]],
+    [17, [2, 4, 5, 7, 9,  11, 13, 16, 18, 20, 21, 24]],
+    [18, [2, 4, 5, 7, 10, 12, 13, 15, 17, 20, 21, 24]],
+    [19, [2, 4, 5, 7, 10, 12, 14, 15, 18, 19, 21, 23]],
+    [20, [1, 3, 5, 8, 10, 11, 14, 15, 18, 19, 22, 24]],
+    [21, [2, 4, 5, 7, 9,  11, 14, 15, 18, 19, 22, 24]],
+    [22, [2, 4, 5, 8, 9,  12, 13, 15, 17, 19, 22, 24]],
+    [23, [2, 4, 5, 8, 9,  12, 13, 16, 18, 19, 21, 23]]
+])
+def test_generate_sample_basic_stroop(sequence_number, expected_solution):
+    block = fully_cross_block([color, text, congruency],
+                              [color, text],
+                              [])
+    enumerator = UCSolutionEnumerator(block)
+    assert enumerator.generate_sample(sequence_number) == expected_solution
+
+
+@pytest.mark.parametrize('sequence_number, expected_solution', [
+    [0,  [1, 5, 3, 7, 12, 10, 14, 17, 16, 20, 24, 21]],
+    [1,  [1, 6, 4, 7, 11, 9,  14, 17, 16, 20, 24, 21]],
+    [2,  [1, 6, 4, 8, 11, 10, 13, 17, 15, 20, 24, 21]],
+    [3,  [1, 6, 4, 8, 11, 10, 14, 18, 15, 19, 23, 21]],
+    [4,  [1, 5, 3, 8, 11, 10, 13, 18, 16, 20, 24, 21]],
+    [5,  [2, 5, 4, 7, 11, 9,  13, 18, 16, 20, 24, 21]],
+    [6,  [2, 5, 4, 7, 12, 10, 13, 17, 15, 20, 24, 21]],
+    [7,  [2, 5, 4, 7, 12, 10, 14, 18, 15, 19, 23, 21]],
+    [8,  [1, 5, 3, 8, 11, 10, 14, 18, 15, 19, 24, 22]],
+    [9,  [2, 5, 4, 7, 11, 9,  14, 18, 15, 19, 24, 22]],
+    [10, [2, 5, 4, 8, 12, 9,  13, 17, 15, 19, 24, 22]],
+    [11, [2, 5, 4, 8, 12, 9,  13, 18, 16, 19, 23, 21]],
+    [12, [1, 5, 3, 7, 12, 10, 14, 18, 15, 20, 23, 22]],
+    [13, [1, 6, 4, 7, 11, 9,  14, 18, 15, 20, 23, 22]],
+    [14, [1, 6, 4, 8, 12, 9,  13, 17, 15, 20, 23, 22]],
+    [15, [1, 6, 4, 8, 12, 9,  14, 17, 16, 19, 23, 21]],
+    [16, [1, 5, 3, 8, 12, 9,  13, 18, 16, 20, 23, 22]],
+    [17, [2, 6, 3, 7, 11, 9,  13, 18, 16, 20, 23, 22]],
+    [18, [2, 6, 3, 7, 12, 10, 13, 17, 15, 20, 23, 22]],
+    [19, [2, 6, 3, 7, 12, 10, 14, 17, 16, 19, 23, 21]],
+    [20, [1, 5, 3, 8, 12, 9,  14, 17, 16, 19, 24, 22]],
+    [21, [2, 6, 3, 7, 11, 9,  14, 17, 16, 19, 24, 22]],
+    [22, [2, 6, 3, 8, 11, 10, 13, 17, 15, 19, 24, 22]],
+    [23, [2, 6, 3, 8, 11, 10, 13, 18, 16, 19, 23, 21]]
+])
+def test_generate_sample_basic_stroop_variation(sequence_number, expected_solution):
+    block = fully_cross_block([color, text, congruency],
+                              [color, congruency],
+                              [])
+    enumerator = UCSolutionEnumerator(block)
+    assert enumerator.generate_sample(sequence_number) == expected_solution
