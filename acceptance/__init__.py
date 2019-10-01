c7c32febeec4e158ef6ca8bee6c7cee71965e944
Maia Hamin <mhamin@princeton.edu>
2019-10-01 10:10:32 -0400

    All

--- acceptance/__init__.py
+++ acceptance/__init__.py
@@ -12,14 +12,20 @@ def __assert_atmostkinarow_pair(k: int, level: Tuple[Factor, Union[SimpleLevel, 
     for e in experiments:
         assert sublist not in [e[level[0]][i:i+k+1] for i in range(len(e[level[0]]) - (k + 1))]
 
+def __assert_atmostkinarow_factor(k: int, f: Factor, experiments: List[dict]) -> None:
+    for l in f.levels:
+        sublist = list(repeat(l[1], k + 1))
+        for e in experiments:
+            assert sublist not in [e[level[0]][i:i+k+1] for i in range(len(e[level[0]]) - (k + 1))]
+
 
 def assert_atmostkinarow(c: AtMostKInARow, experiments: List[dict]) -> None:
     if isinstance(c.level, Factor):
+        __assert_atmostkinarow_factor(c.k, c.level, experiments)
+    else:
         level_tuples = get_all_levels([c.level])
         for t in level_tuples:
             __assert_atmostkinarow_pair(c.k, t, experiments)
-    else:
-        __assert_atmostkinarow_pair(c.k, c.level, experiments)
 
 
 def assert_no_repetition(experiments: List[dict]) -> None:
