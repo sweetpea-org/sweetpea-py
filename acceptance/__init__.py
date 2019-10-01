751a4d6d51e7dc9838f0b4479d5403a71950ec82
Maia Hamin <maia@Maias-MacBook-Pro.local>
2019-10-01 13:10:24 -0400

    Adding testing util

--- acceptance/__init__.py
+++ acceptance/__init__.py
@@ -13,11 +13,10 @@ def __assert_atmostkinarow_pair(k: int, level: Tuple[Factor, Union[SimpleLevel, 
         assert sublist not in [e[level[0]][i:i+k+1] for i in range(len(e[level[0]]) - (k + 1))]
 
 def __assert_atmostkinarow_factor(k: int, f: Factor, experiments: List[dict]) -> None:
-    for l in f.levels:
-        sublist = list(repeat(l[1], k + 1))
+    for level in f.levels:
+        sublist = list(repeat(level, k + 1))
         for e in experiments:
             assert sublist not in [e[level[0]][i:i+k+1] for i in range(len(e[level[0]]) - (k + 1))]
-
 
 def assert_atmostkinarow(c: AtMostKInARow, experiments: List[dict]) -> None:
     if isinstance(c.level, Factor):
@@ -26,7 +25,6 @@ def assert_atmostkinarow(c: AtMostKInARow, experiments: List[dict]) -> None:
         level_tuples = get_all_levels([c.level])
         for t in level_tuples:
             __assert_atmostkinarow_pair(c.k, t, experiments)
-
 
 def assert_no_repetition(experiments: List[dict]) -> None:
     for seq in experiments:
