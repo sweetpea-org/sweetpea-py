0776aeedbefedc38d7c27132a6426d815bc5421d
Pierce Darragh <pierce.darragh@gmail.com>
2020-09-25 15:49:08 -0600

    Implement compatibility for Data.List

---
+++ sweetpea_core/haskell/data/list.py
@@ -0,0 +1,13 @@
+from typing import List, TypeVar
+
+
+T = TypeVar('T')
+
+
+def chunks_of(chunk_size: int, from_list: List[T]) -> List[List[T]]:
+    prev = 0
+    to_list = []
+    while prev < len(from_list):
+        to_list.append(from_list[prev:prev + chunk_size])
+        prev += chunk_size
+    return to_list
