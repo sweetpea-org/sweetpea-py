61c20fc33010dcd308736d7cf16c58b0238d34d5
Pierce Darragh <pierce.darragh@gmail.com>
2020-09-29 15:14:16 -0600

    Correct compatibility naming of Data.List to include Data.List.Split

--- sweetpea_core/haskell/data/list/__init__.py
+++ sweetpea_core/haskell/data/list/__init__.py
@@ -1,21 +1,12 @@
 from typing import Callable, List, TypeVar
 
 
-__all__ = ['chunks_of', 'concat', 'zip_with']
+__all__ = ['concat', 'zip_with']
 
 
 T = TypeVar('T')
 U = TypeVar('U')
 V = TypeVar('V')
-
-
-def chunks_of(chunk_size: int, from_list: List[T]) -> List[List[T]]:
-    prev = 0
-    to_list = []
-    while prev < len(from_list):
-        to_list.append(from_list[prev:prev + chunk_size])
-        prev += chunk_size
-    return to_list
 
 
 def concat(xss: List[List[T]]) -> List[T]:
