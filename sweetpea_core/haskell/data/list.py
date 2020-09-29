e250efb9de22b8de4cec43dd2a98f01475396ef1
Pierce Darragh <pierce.darragh@gmail.com>
2020-09-29 11:38:07 -0600

    Add additional Data.List compatibility functions and define exports for data.list

--- sweetpea_core/haskell/data/list.py
+++ sweetpea_core/haskell/data/list.py
@@ -1,7 +1,12 @@
-from typing import List, TypeVar
+from typing import Callable, List, TypeVar
+
+
+__all__ = ['chunks_of', 'concat', 'zip_with']
 
 
 T = TypeVar('T')
+U = TypeVar('U')
+V = TypeVar('V')
 
 
 def chunks_of(chunk_size: int, from_list: List[T]) -> List[List[T]]:
@@ -11,3 +16,18 @@ def chunks_of(chunk_size: int, from_list: List[T]) -> List[List[T]]:
         to_list.append(from_list[prev:prev + chunk_size])
         prev += chunk_size
     return to_list
+
+
+def concat(xss: List[List[T]]) -> List[T]:
+    return [x for xs in xss for x in xs]
+
+
+def zip_with(transformer_function: Callable[[T, U], V], ts: List[T], us: List[U]) -> List[V]:
+    ts_len = len(ts)
+    us_len = len(us)
+    index = 0
+    result: List[V] = []
+    while index < ts_len and index < us_len:
+        result.append(transformer_function(ts[index], us[index]))
+        index += 1
+    return result
