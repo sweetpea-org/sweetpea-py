875cc964005fbfb3d59136c5de1f447a77a52f5a
Pierce Darragh <pierce.darragh@gmail.com>
2020-10-14 09:51:17 -0600

    Implement simple Haskell.Prelude compatibility

---
+++ sweetpea_core/haskell/prelude.py
@@ -0,0 +1,25 @@
+import operator
+
+from functools import reduce
+from typing import List, Tuple, TypeVar
+
+
+__all__ = ['fst', 'snd', 'sequence']
+
+
+T = TypeVar('T')
+U = TypeVar('U')
+
+
+def fst(pair: Tuple[T, U]) -> T:
+    return pair[0]
+
+
+def snd(pair: Tuple[T, U]) -> U:
+    return pair[1]
+
+
+def sequence(xss: List[List[T]]) -> List[List[T]]:
+    if not xss:
+        return [[]]
+    return [[x] + xs for x in xss[0] for xs in sequence(xss[1:])]
