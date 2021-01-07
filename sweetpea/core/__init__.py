62a22ce97a5c2572270bf3aeb3167bb794a4b7ce
Pierce Darragh <pierce.darragh@gmail.com>
2021-01-07 15:19:36 -0700

    Stub high-level API for integrated Python core as sweetpea.core

---
+++ sweetpea/core/__init__.py
@@ -0,0 +1,66 @@
+"""The behind-the-scenes stuff that powers SweetPea."""
+
+from enum import Enum, auto
+from typing import List, NamedTuple, NewType
+
+
+Var = NewType('Var', int)
+CNF = List[List[Var]]
+
+
+class SolutionSpec(NamedTuple):
+    assignment: List[int]
+    frequency: int
+
+
+class GenerationType(Enum):
+    """An enum that provides access to the three methods of CNF generation:
+
+        1. Assert k == n.
+        2. Assert k < n.
+        3. Assert k > n.
+
+    Instances of this enum also have the `.function` property, which allows
+    calling the associated function.
+    """
+    EQ = auto()
+    LT = auto()
+    GT = auto()
+
+    # NOTE: There is a more complete definition of this enum coming, but its
+    #       useful public interface will not change.
+
+
+class GenerationRequest(NamedTuple):
+    generationType: GenerationType
+    k: int
+    boolean_values: List[Var]
+
+
+def generate_CNF(
+        initial_CNF: CNF,
+        fresh: int,
+        support: int,
+        generation_requests: List[GenerationRequest],
+        use_docker: bool = True
+) -> List[SolutionSpec]:
+    raise NotImplementedError()
+
+
+def generate_non_uniform_CNF(
+        count: int,
+        initial_CNF: CNF,
+        fresh: int,
+        support: int,
+        generation_requests: List[GenerationRequest]
+) -> List[SolutionSpec]:
+    raise NotImplementedError()
+
+
+def build_CNF(
+        initial_CNF: CNF,
+        fresh: int,
+        support: int,
+        generation_requests: List[GenerationRequest]
+) -> str:
+    raise NotImplementedError()
