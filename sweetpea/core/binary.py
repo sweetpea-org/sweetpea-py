aba325ece940f9d10f7c498e4c14145901ff5507
Pierce Darragh <pierce.darragh@gmail.com>
2021-01-23 23:56:16 -0700

    core/binary: implement binary number conversion

---
+++ sweetpea/core/binary.py
@@ -0,0 +1,34 @@
+"""This module provides aliases for values used in representing numbers in a
+binary format.
+"""
+
+
+from typing import List, Literal
+
+
+__all__ = ['Bit', 'BinaryNumber']
+
+
+Bit = Literal[-1, 1]
+BinaryNumber = List[Bit]
+
+
+def binary(value: int) -> BinaryNumber:
+    """Converts an integer value to binary where the output representation is a
+    list of binary digits of the alphabet {-1, 1} (i.e., similar to a normal
+    binary representation except the "false" value is -1 instead of 0).
+
+    For example:
+
+        2  => [1, -1]       == 0b10
+        11 => [1, -1, 1, 1] == 0b1011
+    """
+    output: BinaryNumber = []
+    while value != 0:
+        if value % 2 == 0:
+            output.append(-1)
+        else:
+            output.append(1)
+        value //= 2
+    output.reverse()
+    return output
