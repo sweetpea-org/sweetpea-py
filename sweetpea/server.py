83cdc420e095b7af0f63f668db4dc4c3b3ede01a
Pierce Darragh <pierce.darragh@gmail.com>
2021-02-23 14:10:46 -0700

    sweetpea/server: rewrite build_cnf to use new version
    Also update calls to build_cnf and related usages.

--- sweetpea/server.py
+++ sweetpea/server.py
@@ -1,39 +1,18 @@
-import json
-import requests
-import tempfile
-import time
-import os
-from typing import List
-
 from sweetpea.blocks import Block
-from sweetpea.logic import And, cnf_to_json
-from sweetpea.core import build_CNF
+from sweetpea.core import CNF
 
-"""
-Contains helper functions for interacting with the backend.
 
-"""
-
-"""
-Sends a job to the backend to build the CNF for a design, returning a dict like so:
-{
-    'id': '<job id>',
-    'cnf_str': '<CNF String>'
-}
-"""
-def build_cnf(block: Block) -> dict:
-
+def build_cnf(block: Block) -> CNF:
+    """Converts a Block into a CNF represented as a Unigen-compatible string.
+    """
     backend_request = block.build_backend_request()
-    job_result_str = build_CNF(backend_request.get_cnfs_as_json(),
-        backend_request.fresh - 1,
-        block.variables_per_sample(),
-        backend_request.get_requests_as_generation_requests())
+    cnf = CNF(backend_request.get_cnfs_as_json())
+    return cnf
 
-    return job_result_str        
 
 # TODO: Make a 'local' version of this for better performance?
 # (Invoke solver directly, rather than through docker)
-# Make sure there is working local function 
+# Make sure there is working local function
 # def is_cnf_still_sat(cnf_id: str, additional_clauses: List[And]) -> bool:
 #     request_data = {
 #         'action': 'IsSAT',
