552fd9f73ab496c8efe4db6944015c778ff33ae5
Pierce Darragh <pierce.darragh@gmail.com>
2021-02-23 21:34:45 -0700

    core/original_port: remove original port material

--- sweetpea/core/original_port/__init__.py
+++
@@ -1,7 +0,0 @@
-# This module provides top-level exports for `sweetpea_core`.
-
-from .data_structures import CNF
-from .generate_cnf import (
-    GenerationRequest, GenerationType, SolutionSpec,
-    build_CNF, sample_uniform, sample_non_uniform
-)
