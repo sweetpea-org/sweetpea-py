031e11430164d4c148f256bc6b553039de0bccb8
Pierce Darragh <pierce.darragh@gmail.com>
2021-03-24 10:36:52 -0600

    acceptance/*: implement support for marking slow tests for skipping

--- acceptance/test_uniform_combinatoric.py
+++ acceptance/test_uniform_combinatoric.py
@@ -10,6 +10,7 @@ from sweetpea.sampling_strategies.uniform_combinatoric import UCSolutionEnumerat
 
 path_to_test_files = os.path.dirname(os.path.abspath(__file__)) + "/../sweetpea/tests/sampling_strategies/uc-counting-tests/*.py"
 
+@pytest.mark.slow
 @pytest.mark.parametrize('filename', glob.glob(path_to_test_files))
 def test_uniform_combinatoric_is_always_valid(filename):
 
@@ -25,18 +26,17 @@ def test_uniform_combinatoric_is_always_valid(filename):
 
     # Build the CNF for this block
     build_cnf_result = build_cnf(vars()['block'])
-    
+
     # Build the sampler
     enumerator = UCSolutionEnumerator(vars()['block'])
-    
+
     # Generate some samples, make sure they're all SAT.
     sample_count = min(enumerator.solution_count(), 200)
     print("Checking that UC samples are SAT for {}, sample count={}".format(filename, sample_count))
     for s in range(sample_count):
-        sample = enumerator.generate_solution_variables()            
+        sample = enumerator.generate_solution_variables()
         if not cnf_is_satisfiable(build_cnf_result + CNF(cnf_to_json([And(sample)]))):
             failures.append("Found UNSAT solution! Solution={} File={}".format(sample, filename))
 
     if failures:
         pytest.fail('{} failures occurred in SAT checks for UC sampler: {}'.format(len(failures), failures))
-
