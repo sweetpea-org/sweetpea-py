2a7486b82baa0619bd826352ec9c6b00afa82404
Pierce Darragh <pierce.darragh@gmail.com>
2021-03-26 11:52:20 -0600

    docs/source/conf: use Read the Docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,9 @@ author = 'SweetPea Research Group'
 
 # -- General configuration ---------------------------------------------------
 
+# We use the Read the Docs theme for our Sphinx build.
+import sphinx_rtd_theme  # noqa
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -34,6 +37,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,7 +61,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
