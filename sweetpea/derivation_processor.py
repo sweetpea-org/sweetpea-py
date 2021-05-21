2b224a27fba43d02f3c17a9d10ffec61bafb93ba
Pierce Darragh <pierce.darragh@gmail.com>
2021-05-21 13:14:18 -0600

    derivation_processor: update documentation

--- sweetpea/derivation_processor.py
+++ sweetpea/derivation_processor.py
@@ -1,54 +1,57 @@
+"""This module provides functionality for processing derivations."""
+
+
 import operator as op
 
-from typing import Dict, List, Tuple, Union, Any, cast
+from typing import Any, Dict, List, Tuple, cast
 from functools import reduce
-from itertools import product
 
 # TODO: Fix this Derivation name collision.
-from sweetpea.primitives import Derivation as DerivationPrimitive, DerivedFactor, DerivedLevel, Factor, Level, WithinTrial, Transition, Window, get_external_level_name, SimpleLevel
+from sweetpea.primitives import Derivation as DerivationPrimitive, DerivedFactor, DerivedLevel, Level
 from sweetpea.blocks import Block
 from sweetpea.constraints import Derivation
-from sweetpea.internal import chunk_list, get_all_levels
+from sweetpea.internal import chunk_list
 
 
 class DerivationProcessor:
-    """
-    Usage::
 
-        >>> import operator as op
-        >>> color = Factor("color", ["red", "blue"])
-        >>> text  = Factor("text",  ["red", "blue"])
-        >>> conLevel  = DerivedLevel("con", WithinTrial(op.eq, [color, text]))
-        >>> incLevel  = DerivedLevel("inc", WithinTrial(op.ne, [color, text]))
-        >>> conFactor = Factor("congruent?", [conLevel, incLevel])
-        >>> design = [color, text, conFactor]
-        >>> crossing = [color, text]
-        >>> __process_derivations(design, crossing)
-        [Derivation(derivedIdx=4, dependentIdxs=[[0, 2], [1, 3]]), Derivation(derivedIdx=5, dependentIdxs=[[0, 3], [1, 2]])]
-
-    :rtype:
-        returns a list of tuples. Each tuple is structured as:
-        ``(index of the derived level, list of dependent levels)``
-
-    In the example above, the indicies of the design are:
-
-    ===  =============
-    idx  level
-    ===  =============
-    0    color:red
-    1    color:blue
-    2    text:red
-    3    text:blue
-    4    conFactor:con
-    5    conFactor:inc
-    ===  =============
-
-    So the tuple (4, [[0,2], [1,3]]) represents the information that the
-    derivedLevel con is true iff ``(color:red && text:red) || (color:blue &&
-    text:blue)`` by pairing the relevant indicies together.
-    """
     @staticmethod
     def generate_derivations(block: Block) -> List[Derivation]:
+        """Usage::
+
+            >>> import operator as op
+            >>> color = Factor("color", ["red", "blue"])
+            >>> text  = Factor("text",  ["red", "blue"])
+            >>> conLevel  = DerivedLevel("con", WithinTrial(op.eq, [color, text]))
+            >>> incLevel  = DerivedLevel("inc", WithinTrial(op.ne, [color, text]))
+            >>> conFactor = Factor("congruent?", [conLevel, incLevel])
+            >>> design = [color, text, conFactor]
+            >>> crossing = [color, text]
+            >>> block = fully_cross_block(design, crossing, [])
+            >>> DerivationProcessor.generate_derivations(block)
+            [Derivation(derivedIdx=4, dependentIdxs=[[0, 2], [1, 3]]), Derivation(derivedIdx=5, dependentIdxs=[[0, 3], [1, 2]])]
+
+        In the example above, the indicies of the design are:
+
+        ===  =============
+        idx  level
+        ===  =============
+        0    color:red
+        1    color:blue
+        2    text:red
+        3    text:blue
+        4    conFactor:con
+        5    conFactor:inc
+        ===  =============
+
+        So the tuple ``(4, [[0,2], [1,3]])`` represents the information that
+        the derivedLevel con is true iff ``(color:red && text:red) ||
+        (color:blue && text:blue)`` by pairing the relevant indices together.
+
+        :rtype:
+            returns a list of tuples. Each tuple is structured as:
+            ``(index of the derived level, list of dependent levels)``
+        """
         derived_factors: List[DerivedFactor] = [factor for factor in block.design if isinstance(factor, DerivedFactor)]
         accum = []
 
@@ -96,20 +99,23 @@ class DerivationProcessor:
         else:
             return list(chunk_list(level_strings, level.window.width))
 
-
-    """
-    This is a helper function that shifts the idxs of __process_derivations.
-    ie, if its a Transition(op.eq, [color, color]) (ie "repeat" color transition)
-        then the indexes for the levels of color would be like (0, 0), (1, 1)
-        but actually, the window size for a transition is 2, so what we really want is the indicies
-        (0, 5), (1, 6) (assuming there are 4 levels in the design)
-    So this helper function shifts over indices that were meant to be intepretted as being in a subsequent trial.
-    """
     @staticmethod
     def shift_window(indices: List[List[int]],
                      derivation: DerivationPrimitive,
                      trial_size: int
                      ) -> List[List[int]]:
+        """This is a helper function that shifts the indices of
+        :func:`.DerivationProcessor.generate_derivations`.
+
+        E.g., if it's a ``Transition(op.eq, [color, color])`` (i.e., "repeat"
+        color transition) then the indices for the levels of color would be
+        like ``(0, 0)``, ``(1, 1)``, but actually, the window size for a
+        transition is ``2``, so what we really want is the indices ``(0, 5)``,
+        ``(1, 6)`` (assuming there are 4 levels in the design).
+
+        So this helper function shifts over indices that were meant to be
+        interpreted as being in a subsequent trial.
+        """
         if derivation.width == 1:
             return indices
 
@@ -129,7 +135,8 @@ class DerivationProcessor:
         for idx_list in indices:
             sublist_size = len(idx_list) // argc
             sublists = chunk_list(idx_list, sublist_size)
-            shifted_sublists = [reduce(lambda l, idx: l + [idx + len(l) * trial_size], idx_list, []) for idx_list in sublists]
+            shifted_sublists = [reduce(lambda l, idx: l + [idx + len(l) * trial_size], idx_list, [])
+                                for idx_list in sublists]
             shifted_idxs.append(list(reduce(op.add, shifted_sublists, [])))
 
         return shifted_idxs
