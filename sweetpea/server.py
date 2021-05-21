8162d368ece01b8d96f6b85ede22cf945eb1cc4d
Pierce Darragh <pierce.darragh@gmail.com>
2021-05-21 13:40:59 -0600

    server: update documentation

--- sweetpea/server.py
+++ sweetpea/server.py
@@ -1,26 +1,32 @@
+"""This module provides functionality to communicate with the server."""
+
+
+from typing import List
+
 from sweetpea.blocks import Block
 from sweetpea.core import CNF, combine_cnf_with_requests, cnf_is_satisfiable
-from typing import List
 from sweetpea.logic import And, cnf_to_json
+
 
 def build_cnf(block: Block) -> CNF:
     """Converts a Block into a CNF represented as a Unigen-compatible string.
     """
     backend_request = block.build_backend_request()
     cnf = CNF(backend_request.get_cnfs_as_json())
-    combined_cnf = combine_cnf_with_requests(cnf, 
-        backend_request.fresh - 1, 
-        block.variables_per_sample(), 
+    combined_cnf = combine_cnf_with_requests(
+        cnf,
+        backend_request.fresh - 1,
+        block.variables_per_sample(),
         backend_request.get_requests_as_generation_requests())
     return combined_cnf
 
 
 def is_cnf_still_sat(block: Block, additional_clauses: List[And]) -> bool:
-    
     backend_request = block.build_backend_request()
     cnf = CNF(backend_request.get_cnfs_as_json()) + CNF(cnf_to_json(additional_clauses))
-    combined_cnf = combine_cnf_with_requests(cnf, 
-        backend_request.fresh - 1, 
-        block.variables_per_sample(), 
+    combined_cnf = combine_cnf_with_requests(
+        cnf,
+        backend_request.fresh - 1,
+        block.variables_per_sample(),
         backend_request.get_requests_as_generation_requests())
     return cnf_is_satisfiable(combined_cnf)
