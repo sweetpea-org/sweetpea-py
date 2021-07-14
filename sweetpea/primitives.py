39a16cbcd68bde164ead0439bbabb1c26d03cbd8
Pierce Darragh <pierce.darragh@gmail.com>
2021-07-14 14:55:45 -0600

    sweetpea/primitives: implement functions for #10

--- sweetpea/primitives.py
+++ sweetpea/primitives.py
@@ -949,6 +949,8 @@ def get_internal_level_name(level: Level) -> str:
     return level.internal_name
 
 
+#### Class aliases.
+
 #: An alias for :class:`.WithinTrialDerivationWindow`.
 #:
 #: .. deprecated:: 0.1.0
@@ -972,3 +974,142 @@ Transition = TransitionDerivationWindow
 #:     This class will be removed in favor of
 #:     :class:`.DerivationWindow`.
 Window = DerivationWindow
+
+
+#### Noun-form function aliases.
+
+def simple_level(name: str, weight: Optional[int] = None) -> SimpleLevel:
+    """A function alias for :class:`.SimpleLevel`.
+
+    .. deprecated:: 0.1.2
+
+        This function will be removed in favor of direct instantiation of
+        :class:`.SimpleLevel`.
+
+    :param name:
+        The name of the level.
+
+    :param weight:
+        An optional weight for the level.
+    """
+    if weight is None:
+        return SimpleLevel(name)
+    return SimpleLevel(name, weight)
+
+
+def derived_level(name: str, derivation: Window, weight: Optional[int] = None) -> DerivedLevel:
+    """A function alias for :class:`.DerivedLevel`.
+
+    .. deprecated:: 0.1.2
+
+        This function will be removed in favor of direct instantiation of
+        :class:`.DerivedLevel`.
+
+    :param name:
+        The name of the level.
+
+    :param derivation:
+        The derivation window for this level.
+
+    :param weight:
+        An optional weight for the level.
+    """
+    if weight is None:
+        return DerivedLevel(name, derivation)
+    return DerivedLevel(name, derivation, weight)
+
+
+def else_level(name: str, weight: Optional[int] = None) -> ElseLevel:
+    """A function alias for :class:`.ElseLevel`.
+
+    .. deprecated:: 0.1.2
+
+        This function will be removed in favor of direct instantiation of
+        :class:`.ElseLevel`.
+
+    :param name:
+        The name of the level.
+
+    :param weight:
+        An optional weight for the level.
+    """
+    if weight is None:
+        return ElseLevel(name)
+    return ElseLevel(name, weight)
+
+
+def factor(name: str, levels: Sequence[Any]) -> Factor:
+    """A function alias for :class:`.Factor`.
+
+    .. deprecated:: 0.1.2
+
+        This function will be removed in favor of direct instantiation of
+        :class:`.Factor`.
+
+    :param name:
+        The name of the factor.
+
+    :param levels:
+        A sequence of levels for the factor.
+    """
+    return Factor(name, levels)
+
+
+def within_trial(fn: Callable, args: List[Factor]) -> WithinTrialDerivationWindow:
+    """A function alias for :class:`.WithinTrialDerivationWindow`.
+
+    .. deprecated:: 0.1.2
+
+        This function will be removed in favor of direct instantiation of
+        :class:`.WithinTrialDerivationWindow`.
+
+    :param fn:
+        A predicate function. See :class:`.WithinTrialDerivationWindow`.
+
+    :param args:
+        A list of :class:`Factors <.Factor>`. See
+        :class:`.WithinTrialDerivationWindow`.
+    """
+    return WithinTrialDerivationWindow(fn, args)
+
+
+def transition(fn: Callable, args: List[Factor]) -> TransitionDerivationWindow:
+    """A function alias for :class:`.TransitionDerivationWindow`.
+
+    .. deprecated:: 0.1.2
+
+        This function will be removed in favor of direct instantiation of
+        :class:`.TransitionDerivationWindow`.
+
+    :param fn:
+        A predicate function. See :class:`.TransitionDerivationWindow`.
+
+    :param args:
+        A list of :class:`Factors <.Factor>`. See
+        :class:`.TransitionDerivationWindow`.
+    """
+    return TransitionDerivationWindow(fn, args)
+
+
+def window(fn: Callable, args: List[Factor], width: int, stride: int) -> DerivationWindow:
+    """A function alias for :class:`.DerivationWindow`.
+
+    .. deprecated:: 0.1.2
+
+        This function will be removed in favor of direct instantiation of
+        :class:`.DerivationWindow`.
+
+    :param fn:
+        A predicate function. See :class:`.DerivationWindow`.
+
+    :param args:
+        A list of :class:`Factors <.Factor>`. See
+        :class:`.DerivationWindow`.
+
+    :param width:
+        The width of the window.
+
+    :param stride:
+        The stride of the window.
+    """
+    return DerivationWindow(fn, args, width, stride)
