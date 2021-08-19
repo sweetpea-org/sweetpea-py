5004c442ed08e7048ab6242f7a4bf890d00cfa0d
ahsanbutt95 <ahsansbutt@hotmail.com>
2021-08-19 07:04:20 -0600

    new version: 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sweetpea",
-    version="0.1.1",
+    version="0.1.2",
     author="Annie Cherkaev, Ben Draut, Ahsan Sajjad Butt, Pierce Darragh",
     author_email="annie.cherk@gmail.com, drautb@cs.utah.edu, ahsansbutt@hotmail.com",
     description="A language for synthesizing randomized experimental designs",
