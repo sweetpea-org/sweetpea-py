68d5ba772b0c4bf6cd130a5c36dff25271bedd23
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-02-11 20:44:52 -0500

    Update test_auto_correlation_score.py

--- acceptance/test_auto_correlation_score.py
+++ acceptance/test_auto_correlation_score.py
@@ -19,11 +19,23 @@ samples = [
 
 
 def test_score_auto_correlation_all():
+    # this is to prevent testing when sklearn is not installed:
+    try:
+        from sklearn.neural_network import MLPClassifier
+    except ImportError as e:
+        assert True
+        return
     res = auto_correlation_scores_samples(samples)
     assert 'color' in res.keys() and 'word' in res.keys() and 'congruency' in res.keys()
 
 
 def test_score_auto_correlation():
+    # this is to prevent testing when sklearn is not installed:
+    try:
+        from sklearn.neural_network import MLPClassifier
+    except ImportError as e:
+        assert True
+        return
     res = auto_correlation_scores_samples(samples, ['color'])
     assert 'color' in res.keys() and 'word' not in res.keys() and 'congruency' not in res.keys()
 
