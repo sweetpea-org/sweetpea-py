1e357d1ac29ccd0fd3d8f60c72539378fa9d0c69
unknown <dwang0127@gmail.com>
2025-04-17 12:31:14 -0500

    Update TaskSwitch Example with better formatting

--- example_programs/TaskSwitching_challenging.py
+++ example_programs/TaskSwitching_challenging.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append("..")
 
 from sweetpea import (
-    Factor, DerivedLevel, WithinTrial, Transition, AtMostKInARow,
-    CrossBlock, synthesize_trials, print_experiments,
+    Factor, DerivedLevel, WithinTrial, Transition, AtMostKInARow, MinimumTrials,
+    CrossBlock, MultiCrossBlock, synthesize_trials, print_experiments, tabulate_experiments,
     CMSGen, IterateGen, RandomGen
 )
 
@@ -19,12 +19,19 @@ factors (levels):
 - correct response (left, right): dependent factor.
 - congruency (congruent, incongruent): dependent factor.
 - task transition (repetition, switch). dependent factor of task: <-- difference between task transition & tt-type
-- task transition type, dependent on factor task transition of current trial and factor task on previous trial:
 - response transition (repetition, switch). dependent factor of correct response:
 if left-left then task transition = repetition
 if right-right then task transition = repetition
 if left-right then task transition = switch
 if right-left then task transition = switch
+- task transition type (color switch, color repetition, motion switch, motion repetition, size switch, size repetition).
+dependent on factor task transition of current trial and factor task on previous trial:
+color switch: The previous trial was a color task and the current trial is not
+color repetition: The previous and current trial are a color task
+motion switch: The previous trial was a motion task and the current trial is not
+motion repetition: The previous and current trial are a motion task
+size switch: The previous trial was a size task and the current trial is not
+size repetition: The previous and current trial are a size task
 constraints:
 - counterbalancing congruency x response x task x response-transition x color x motion x size x transition type
 - no more than 7 task repetitions in a row
@@ -72,15 +79,15 @@ if current color == blue & current motion == down & size = small then response =
 otherwise incongruent
 """
 
-def congruent(color, motion):
+def congruent(color, motion, size):
     return ((color == "red") and (motion == "up") and (size == "large")) or ((color == "blue") and (motion == "down")  and (size == "small"))
 
-def incongruent(color, motion):
-    return not congruent(color, motion)
+def incongruent(color, motion, size):
+    return not congruent(color, motion, size)
 
 congruency = Factor("congruency", 
-    [DerivedLevel("con", WithinTrial(congruent,   [color, motion])),
-    DerivedLevel("inc", WithinTrial(incongruent, [color, motion]))])
+    [DerivedLevel("con", WithinTrial(congruent,   [color, motion, size])),
+    DerivedLevel("inc", WithinTrial(incongruent, [color, motion, size]))])
 
 
 """
@@ -91,15 +98,15 @@ if size-size     then task transition = repetition
 otherwise switch
 """
 
-def task_repeat(color, motion, size):
-    return (color[0] == color[-1]) or (motion[0] == motion[-1]) or (size[0] == size[-1])
+def task_repeat(tasks):
+    return tasks[0] == tasks[-1]
 
-def task_switch(color, motion, size):
-    return not task_repeat(color, motion, size)
+def task_switch(tasks):
+    return not task_repeat(tasks)
 
 task_transition = Factor("task_transition", [
-    DerivedLevel("repeat", Transition(task_repeat, [color, motion, size])),
-    DerivedLevel("switch", Transition(task_switch, [color, motion, size]))
+    DerivedLevel("repeat", Transition(task_repeat, [task])),
+    DerivedLevel("switch", Transition(task_switch, [task]))
 ])
 
 """
@@ -111,7 +118,36 @@ if task on previous trial = color & task transition on current trial =  repetiti
 if task on previous trial = motion & task transition on current trial =  repetition then task transition type = motion repetition
 if task on previous trial = size & task transition on current trial =  repetition then task transition type = size repetition
 """
-#def task_ currently not used?
+def color_switch(tasks):
+    return tasks[0] != tasks[-1] and tasks[-1] == "color"
+
+def color_repeat(tasks):
+    return tasks[0] == tasks[-1] and tasks[-1] == "color"
+
+def motion_switch(tasks):
+    return tasks[0] != tasks[-1] and tasks[-1] == "motion"
+
+def motion_repeat(tasks):
+    return tasks[0] == tasks[-1] and tasks[-1] == "motion"
+
+def size_switch(tasks):
+    return tasks[0] != tasks[-1] and tasks[-1] == "size"
+
+def size_repeat(tasks):
+    return tasks[0] == tasks[-1] and tasks[-1] == "size"
+
+# Deterministic constraints:
+# color-switch and motion-switch occur twice as frequently as size-switch
+
+task_transition_type = Factor("task_transition_type", [
+    DerivedLevel("color_switch", Transition(color_switch, [task]), 2),
+    DerivedLevel("color_repeat", Transition(color_repeat, [task]), 1),
+    DerivedLevel("motion_switch", Transition(motion_switch, [task]), 2),
+    DerivedLevel("motion_repeat", Transition(motion_repeat, [task]), 1),
+    DerivedLevel("size_switch", Transition(size_switch, [task]), 1),
+    DerivedLevel("size_repeat", Transition(size_repeat, [task]), 1),
+])
+
 
 """
          response transition (repetition, switch). dependent factor of correct response:
@@ -138,14 +174,15 @@ constraints = [AtMostKInARow(k, task_transition),
                AtMostKInARow(k, resp_transition)]
                 
 
-print(type(constraints))
 
-design       = [color, motion, size, task, congruency, response, task_transition, resp_transition]
+design       = [color, motion, size, task, congruency, response, task_transition, task_transition_type, resp_transition]
 
-crossing     = [color, motion, size, task]
+crossing     = [response, task_transition_type]
 block        = CrossBlock(design, crossing, constraints)
 
-experiments  = synthesize_trials(block, 5, CMSGen)
+experiments  = synthesize_trials(block, 1, CMSGen)
 # Could also use IterateGen or RandomGen
 
 print_experiments(block, experiments)
+
+tabulate_experiments(block, experiments, [task_transition_type])
